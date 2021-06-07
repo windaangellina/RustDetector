401eeb4dc061b69ce21bef694f2ba83adb03708c
Winda Angelina Utama <wau959@gmail.com>
2021-06-08 01:08:17 +0700

    testing api down, ngubah posisi code glide with url to get bitmap

--- android/RustDetector/app/src/main/java/com/capstone/rustdetector/ui/home/MainActivity.kt
+++ android/RustDetector/app/src/main/java/com/capstone/rustdetector/ui/home/MainActivity.kt
@@ -4,12 +4,16 @@ import android.app.Activity
 import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.capstone.rustdetector.R
 import com.capstone.rustdetector.databinding.ActivityMainBinding
 import com.capstone.rustdetector.utils.FunctionUtil
@@ -159,11 +163,27 @@ class MainActivity : AppCompatActivity() {
                     event.getContentIfNotHandled().let {
                         if (it != null){
                             val imageUrl = it.url
-                            FunctionUtil.showImageByUrlUsingGlide(
-                                applicationContext,
-                                imageUrl,
-                                binding.layoutPrediction.imageViewResult
-                            )
+//                            FunctionUtil.showImageByUrlUsingGlide(
+//                                applicationContext,
+//                                imageUrl,
+//                                binding.layoutPrediction.imageViewResult
+//                            )
+
+                            FunctionUtil.makeToast(applicationContext, imageUrl)
+                            Glide.with(applicationContext)
+                                .asBitmap()
+                                .load(imageUrl)
+                                .into(object : CustomTarget<Bitmap>(){
+                                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                                        binding.layoutPrediction.imageViewResult.setImageBitmap(resource)
+                                    }
+                                    override fun onLoadCleared(placeholder: Drawable?) {
+                                        // this is called when imageView is cleared on lifecycle call or for
+                                        // some other reason.
+                                        // if you are referencing the bitmap somewhere else too other than this imageView
+                                        // clear it here as you can no longer have the bitmap
+                                    }
+                                })
                         } else{
                             setResultDisplay(noResult = true)
                             FunctionUtil.makeToast(
--- android/RustDetector/app/src/main/java/com/capstone/rustdetector/viewmodel/RustDetectorViewModel.kt
+++ android/RustDetector/app/src/main/java/com/capstone/rustdetector/viewmodel/RustDetectorViewModel.kt
@@ -2,6 +2,7 @@ package com.capstone.rustdetector.viewmodel
 
 import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.capstone.rustdetector.source.RustDetectorRepository
 import com.capstone.rustdetector.source.remote.api.response.CorrosionSegmentationResponse
@@ -10,8 +11,15 @@ import com.capstone.rustdetector.utils.Event
 class RustDetectorViewModel(private val repository: RustDetectorRepository) : ViewModel() {
     val loadingStatus : LiveData<Boolean> = repository.isLoading
 
-    fun getCorrosionSegmentationResult(bitmapSelectedPhoto: Bitmap, fileName : String) :
+    fun getCorrosionSegmentationResult(bitmapSelectedPhoto: Bitmap, fileName: String):
             LiveData<Event<CorrosionSegmentationResponse?>> {
-        return repository.getSegmentationResult(bitmap = bitmapSelectedPhoto, fileName = fileName)
+        //return repository.getSegmentationResult(bitmap = bitmapSelectedPhoto, fileName = fileName)
+
+        val testingResult = CorrosionSegmentationResponse(
+            "https://assets.pikiran-rakyat.com/crop/67x118:655x668/x/photo/2020/12/06/2809916107.png"
+        )
+        val mutable = MutableLiveData<Event<CorrosionSegmentationResponse?>>()
+        mutable.postValue(Event(testingResult))
+        return mutable
     }
 }
